a77907c86a42955d5a513b6394be526c8d011065
sirgrimorum <andres.espinosa@grimorum.com>
2018-02-28 14:04:18 -0500

    Hacer que la clase de notificaciones para enviar una alerta sea dinámica

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -90,53 +90,58 @@ class CrudGeneratorServiceProvider extends ServiceProvider {
          * Console commands
          */
         Artisan::command('crudgen:sendalert ', function () {
-            $email = $this->anticipate("User email?", ['andres.espinosa@grimorum.com']);
-            if ($toUser = \App\User::where("email", "=", $email)->first()) {
-                $message = $this->ask("Message?");
-                /* $options = array(
-                  'cluster' => 'us2',
-                  'encrypted' => true
-                  );
-                  $pusher = new Pusher(
-                  'adf503a8756876656ec6', 'b21a0ee639fbb893e41d', '481076', $options
-                  );
-                 */
-                $data = [];
-                $data['message'] = $message;
-                /* $result = $pusher->trigger('my-channel', 'my-event', $data, null, true);
-                  $this->line(print_r($result, true)); */
-                $tiempo = $this->ask("Send in? (Just a number,next you will select seconds, minutes, etc.)");
-                if (!is_int((int) $tiempo)) {
-                    $tiempo = $this->ask("Send in? HAS TO BE AN INTEGER NUMBER");
-                }
-                if (is_int((int) $tiempo)) {
-                    $unidad = $this->choice('...', ['seconds', 'minutes', 'hours', 'days'], 0);
-                    switch ($unidad) {
-                        case 'seconds':
-                            $when = now()->addSeconds($tiempo);
-                            break;
-                        case 'minutes':
-                            $when = now()->addMinutes($tiempo);
-                            break;
-                        case 'hours':
-                            $when = now()->addHours($tiempo);
-                            break;
-                        case 'days':
-                            $when = now()->addDays($tiempo);
-                            break;
-                        default:
-                            $when = now()->addMinutes($tiempo);
-                            break;
+            $alertaClass = $this->ask("Notification Class?");
+            if (class_exists($alertaClass)) {
+                $email = $this->anticipate("User email?", ['andres.espinosa@grimorum.com']);
+                if ($toUser = \App\User::where("email", "=", $email)->first()) {
+                    $message = $this->ask("Message?");
+                    /* $options = array(
+                      'cluster' => 'us2',
+                      'encrypted' => true
+                      );
+                      $pusher = new Pusher(
+                      'adf503a8756876656ec6', 'b21a0ee639fbb893e41d', '481076', $options
+                      );
+                     */
+                    $data = [];
+                    $data['message'] = $message;
+                    /* $result = $pusher->trigger('my-channel', 'my-event', $data, null, true);
+                      $this->line(print_r($result, true)); */
+                    $tiempo = $this->ask("Send in? (Just a number,next you will select seconds, minutes, etc.)");
+                    if (!is_int((int) $tiempo)) {
+                        $tiempo = $this->ask("Send in? HAS TO BE AN INTEGER NUMBER");
                     }
-                    $toUser->notify((new \App\Notifications\Alerta($data))
-                                    ->delay($when)
-                    );
-                    $this->info("Listo!");
+                    if (is_int((int) $tiempo)) {
+                        $unidad = $this->choice('...', ['seconds', 'minutes', 'hours', 'days'], 0);
+                        switch ($unidad) {
+                            case 'seconds':
+                                $when = now()->addSeconds($tiempo);
+                                break;
+                            case 'minutes':
+                                $when = now()->addMinutes($tiempo);
+                                break;
+                            case 'hours':
+                                $when = now()->addHours($tiempo);
+                                break;
+                            case 'days':
+                                $when = now()->addDays($tiempo);
+                                break;
+                            default:
+                                $when = now()->addMinutes($tiempo);
+                                break;
+                        }
+                        $toUser->notify((new $alertaClass($data))
+                                        ->delay($when)
+                        );
+                        $this->info("Listo!");
+                    } else {
+                        $this->error("Paila!");
+                    }
                 } else {
-                    $this->error("Paila!");
+                    $this->error("User not found");
                 }
             } else {
-                $this->error("User not found");
+                $this->error("Class not found");
             }
         })->describe('Broadcast an alert to a user');
 
@@ -232,7 +237,7 @@ class CrudGeneratorServiceProvider extends ServiceProvider {
                 $config = CrudGenerator::getConfig($model, true, '', '', false);
             } else {
                 $this->line("not mergin");
-                $config = CrudGenerator::getConfig($model, false, '', '', false, true,true);
+                $config = CrudGenerator::getConfig($model, false, '', '', false, true, true);
             }
 
             $bar->advance();
