de296b482cd5f5e52136ad2dcde9db6f48e21636
SirGrimorum <andres.espinosa@grimorum.com>
2020-08-24 11:10:02 -0500

    soportar iframe en ckeditor

--- src/Assets/ckeditor/config.js
+++ src/Assets/ckeditor/config.js
@@ -3,42 +3,42 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function (config) {
+CKEDITOR.editorConfig = function(config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.contentsLanguage = 'fr';
-    config.skin='office2013';//flat, office2013, kama, moono, minimalist, icy_orange, atlas, prestige
+    config.skin = 'office2013'; //flat, office2013, kama, moono, minimalist, icy_orange, atlas, prestige
     //config.uiColor = '#bbbbbb';
     config.toolbarCanCollapse = true;
     config.extraPlugins = 'ckawesome';
     //config.allowedContent = true;
-    
-    config.extraAllowedContent = 'div(*)[id,data-href,data-parent,data-target,data-toggle,aria-expanded,aria-controls,role,data-ride,style]; ol(*); ul(*)[class]; li(*)[data-target,data-slide-to,style]; p(*); h3(*); h4(*); a(*)[data-slide,data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; button(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; span(*)[aria-hidden,style];*[id,class]';
+
+    config.extraAllowedContent = 'div(*)[id,data-href,data-parent,data-target,data-toggle,aria-expanded,aria-controls,role,data-ride,style]; ol(*); ul(*)[class]; li(*)[data-target,data-slide-to,style]; p(*); h3(*); h4(*); a(*)[data-slide,data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; button(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; span(*)[aria-hidden,style];iframe(*)[width,height,src,frameborder,allow,allowfullscreen,class];*[id,class]';
     config.contentsCss = urlAssetsCkEditor;
     config.fontawesomePath = urlAssetsCkEditor;
-    
-    //config.extraAllowedContent = 'div(col-md-*,container*,row, panel*,well*,list*,active*,collapse*,data*,role*),p(list*),h3(list*),h4(list*),a(list*,collapse*)';
-    
-    
-    config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		'/',
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert','ckawesome'] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
 
-	config.removeButtons = 'Save,NewPage,Print,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,BidiLtr,BidiRtl,Flash,PageBreak,About';
+    //config.extraAllowedContent = 'div(col-md-*,container*,row, panel*,well*,list*,active*,collapse*,data*,role*),p(list*),h3(list*),h4(list*),a(list*,collapse*)';
+
+
+    config.toolbarGroups = [
+        { name: 'document', groups: ['mode', 'document', 'doctools'] },
+        { name: 'clipboard', groups: ['clipboard', 'undo'] },
+        { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+        { name: 'forms', groups: ['forms'] },
+        '/',
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+        '/',
+        { name: 'styles', groups: ['styles'] },
+        { name: 'colors', groups: ['colors'] },
+        '/',
+        { name: 'links', groups: ['links'] },
+        { name: 'insert', groups: ['insert', 'ckawesome'] },
+        { name: 'tools', groups: ['tools'] },
+        { name: 'others', groups: ['others'] },
+        { name: 'about', groups: ['about'] }
+    ];
+
+    config.removeButtons = 'Save,NewPage,Print,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,BidiLtr,BidiRtl,Flash,PageBreak,About';
 
 };
--- src/Assets/ckeditor/config_full.js
+++ src/Assets/ckeditor/config_full.js
@@ -3,13 +3,13 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function (config) {
+CKEDITOR.editorConfig = function(config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
 
     //config.allowedContent = true;
-    config.extraAllowedContent = 'div(*)[data-href,data-parent,data-target,data-toggle,aria-expanded,aria-controls,role]; p(*); h3(*); h4(*); a(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; button(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; span(*)[class,aria-hidden,style]';
+    config.extraAllowedContent = 'div(*)[data-href,data-parent,data-target,data-toggle,aria-expanded,aria-controls,role]; p(*); h3(*); h4(*); a(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; button(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; span(*)[class,aria-hidden,style];iframe(*)[width,height,src,frameborder,allow,allowfullscreen,class]';
     config.contentsCss = [urlAssets + "js/bootstrap-3.3.7/css/bootstrap.min.css", urlAssets + "css/principal.css"];
     //config.extraAllowedContent = 'div(col-md-*,container*,row, panel*,well*,list*,active*,collapse*,data*,role*),p(list*),h3(list*),h4(list*),a(list*,collapse*)';
 
