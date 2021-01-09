8df4e7105e9280345b61765bb8b5e049f5cc7fbb
SirGrimorum <andres.espinosa@grimorum.com>
2021-01-09 12:07:00 -0500

    Cargar botones con url cuando no se han definido

--- src/Views/admin/index/content.blade.php
+++ src/Views/admin/index/content.blade.php
@@ -28,12 +28,12 @@ if (Lang::has("crudgenerator::" . strtolower($modelo) . ".labels.singular")) {
 if (($textConfirm = trans('crudgenerator::' . strtolower($modelo) . '.messages.confirm_destroy')) == 'crudgenerator::' . strtolower($modelo) . '.mensajes.confirm_destroy') {
     $textConfirm = trans('crudgenerator::admin.messages.confirm_destroy');
 }
-$config['botones'] = [
+/*$config['botones'] = [
     'show' => "<a class='btn btn-info' href='" . url($base_url . "/" . strtolower($modelo) . "/:modelId") . "' title='" . trans('crudgenerator::datatables.buttons.t_show') . " " . $singulares . "'>". trans("crudgenerator::datatables.buttons.show") . "</a>",
     'edit' => "<a class='btn btn-success' href='" . url($base_url . "/" . strtolower($modelo) . "/:modelId/edit") . "' title='" . trans('crudgenerator::datatables.buttons.t_edit') . " " . $singulares . "'>". trans("crudgenerator::datatables.buttons.edit") . "</a>",
     'remove' => "<a class='btn btn-danger' href='" . url($base_url . "/" . strtolower($modelo) . "/:modelId/destroy") . "' data-confirm='" . $textConfirm . "' data-yes='" . trans('crudgenerator::admin.layout.labels.yes') . "' data-no='" . trans('crudgenerator::admin.layout.labels.no') . "' data-confirmtheme='" . config('sirgrimorum.crudgenerator.confirm_theme') . "' data-confirmicon='" . config('sirgrimorum.crudgenerator.icons.confirm') . "' data-confirmtitle='' data-method='delete' rel='nofollow' title='" . trans('crudgenerator::datatables.buttons.t_remove') . " " . $plurales . "'>". trans("crudgenerator::datatables.buttons.remove") . "</a>",
     'create' => "<a class='btn btn-info' href='" . url($base_url . "/" . strtolower($modelo) . "s/create") . "' title='" . trans('crudgenerator::datatables.buttons.t_create') . " " . $singulares . "'>". trans("crudgenerator::datatables.buttons.create") . "</a>",
-];
+];*/
 /*$config['botones'] = [
     'show' => url($base_url . "/" . strtolower($modelo) . "/:modelId"),
     'edit' => url($base_url . "/" . strtolower($modelo) . "/:modelId/edit"),
--- src/Views/admin/index/modal.blade.php
+++ src/Views/admin/index/modal.blade.php
@@ -21,11 +21,11 @@ if (Lang::has("crudgenerator::" . strtolower($modelo) . ".labels.singular")) {
                 if (($textConfirm = trans('crudgenerator::' . strtolower($modelo) . '.mensajes.confirm_destroy')) == 'crudgenerator::' . strtolower($modelo) . '.mensajes.confirm_destroy') {
                     $textConfirm = trans('crudgenerator::admin.mensajes.confirm_destroy');
                 }
-                $config['botones'] = [
+                /*$config['botones'] = [
                     "<a class='btn btn-info' href='" . url($base_url . "/" . strtolower($modelo) . "/:modelId") . "' title='" . trans('crudgenerator::admin.layout.ver') . "'><span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span></a>",
                     "<a class='btn btn-success' href='" . url($base_url . "/" . strtolower($modelo) . "/:modelId/edit") . "' title='" . trans('crudgenerator::admin.layout.editar') . "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></a>",
                     "<a class='btn btn-danger' href='" . url($base_url . "/" . strtolower($modelo) . "/:modelId/destroy") . "' data-confirm='" . $textConfirm . "' data-yes='" . $textConfirm = trans('crudgenerator::admin.layout.labels.yes') . "' data-yes='" . $textConfirm = trans('crudgenerator::admin.layout.labels.no') . "' data-confirmtheme='" . config('sirgrimorum.crudgenerator..confirm_theme') . "' data-confirmicon='" . config('sirgrimorum.crudgenerator.icons.confirm') . "' data-method='delete' rel='nofollow' title='" . trans('crudgenerator::admin.layout.borrar') . "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></a>",
-                ];
+                ];*/
                 ?>
                     {!! CrudGenerator::lists($config,false,true) !!}
             </div>
