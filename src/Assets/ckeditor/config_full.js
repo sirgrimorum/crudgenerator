62f83b67c23d94ae000b4b860d338b91519edb6d
SirGrimorum <andres.espinosa@grimorum.com>
2021-02-26 07:27:51 -0500

    aceptar clases en h1, h2 y h5

--- src/Assets/ckeditor/config_full.js
+++ src/Assets/ckeditor/config_full.js
@@ -9,7 +9,7 @@ CKEDITOR.editorConfig = function(config) {
     // config.uiColor = '#AADC6E';
 
     //config.allowedContent = true;
-    config.extraAllowedContent = 'div(*)[data-href,data-parent,data-target,data-toggle,aria-expanded,aria-controls,role]; p(*); h3(*); h4(*); a(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; button(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; span(*)[class,aria-hidden,style];iframe(*)[width,height,src,frameborder,allow,allowfullscreen,class]';
+    config.extraAllowedContent = 'div(*)[data-href,data-parent,data-target,data-toggle,aria-expanded,aria-controls,role]; p(*); h1(*); h2(*); h3(*); h4(*); h5(*); a(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; button(*)[data-target,data-parent,data-toggle,aria-expanded,aria-controls,role]; span(*)[class,aria-hidden,style];iframe(*)[width,height,src,frameborder,allow,allowfullscreen,class]';
     config.contentsCss = [urlAssets + "js/bootstrap-3.3.7/css/bootstrap.min.css", urlAssets + "css/principal.css"];
     //config.extraAllowedContent = 'div(col-md-*,container*,row, panel*,well*,list*,active*,collapse*,data*,role*),p(list*),h3(list*),h4(list*),a(list*,collapse*)';
 
